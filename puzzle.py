1e202e4aac2850d1892f209a333f3688a7f404c2
MarkusViktoria <149368207+MarkusViktoria@users.noreply.github.com>
2023-11-06 13:04:08 +0200

    Create puzzle.py

---
+++ puzzle.py
@@ -0,0 +1,37 @@
+''' validate_board'''
+
+def validate_rows(board: list) -> bool:
+    '''
+    Return result of checking rows.
+    >>> validate_rows(["**** ****", "***1 ****", "**  3****", "* 4 1****",\
+ "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    True
+        '''
+    pass
+def validate_columns(board: list) -> bool:
+    '''
+    Return result of checking rows.
+    >>> validate_columns(["**** ****", "***1 ****", "**  3****", "* 4 1****",\
+ "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    False
+        '''
+    pass
+
+def validate_colors(board: list) -> bool:
+    '''
+    Return result of checking colored blocks.
+    >>> validate_colors(["**** ****", "***1 ****", "**  3****", "* 4 1****",\
+ "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    True
+    '''
+    pass
+
+def validate_board(board: list) -> bool:
+    '''
+    Return result of checking board.
+    >>> validate_board(["**** ****", "***1 ****", "**  3****", "* 4 1****",\
+ "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    False
+    '''
+    pass
+  
